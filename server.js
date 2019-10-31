8882fffd07f60a74281f77d8190d8425fbb5f881
Eli Vargas <eevargas@gmail.com>
2019-10-31 18:36:36 -0500

    Add auth libs and packages

--- server.js
+++ server.js
@@ -3,17 +3,19 @@ const express = require("express");
 const mongoose = require("mongoose");
 const routes = require("./routes");
 const app = express();
+const jwtAuth = require('./lib/jwt-auth')
 const PORT = process.env.PORT || 3001;
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(jwtAuth.middleware)
+app.use(routes);
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
-app.use(routes);
 
 // Connect to the Mongo DB
 mongoose.connect(process.env.MONGODB_URI || "mongodb://127.0.0.1/maintain");
