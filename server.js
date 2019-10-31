d6e90e9af9f67aade6968051ee2343891f57ee28
Eli Vargas <eevargas@gmail.com>
2019-10-31 18:25:54 -0500

    fix local mongo uri

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ if (process.env.NODE_ENV === "production") {
 app.use(routes);
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/maintainmongodb://maintain:@ds131905.mlab.com:31905/heroku_ppgvlmlw");
+mongoose.connect(process.env.MONGODB_URI || "mongodb://127.0.0.1/maintain");
 
 // Start the API server
 app.listen(PORT, function () {
