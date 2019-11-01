68230ce21f40e074f787596cad5b83e31d565b46
Eli Vargas <eevargas@gmail.com>
2019-10-31 20:30:29 -0500

    Add auth test endpoint

--- routes/api/health.js
+++ routes/api/health.js
@@ -8,7 +8,32 @@ const journalController = require("../../controllers/Journal");
 // Retrieve books saved from Journal
 // GET /api/journal
 router.get("/", (req, res) => {
-    res.json({ status: 'OK' })
+    res.json({
+        status: 'OK',
+        data: null,
+        detail: null,
+        code: 200
+    })
+});
+
+// Protected endpoints
+router.get("/auth", (req, res) => {
+    const payload = req.authorize()
+    if (!payload) {
+        res.status(401).json({
+            status: 'Unauthorized',
+            data: null,
+            detail: "Bad Token",
+            code: 401
+        });
+    }
+
+    res.json({
+        status: 'OK',
+        data: payload,
+        detail: null,
+        code: 200
+    })
 });
 
 // Exporting router
