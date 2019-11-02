c6a31e88af7ceab1eb68bff608a3cd888e470f46
ashleyerffmeyer <ashley.erffmeyer@gmail.com>
2019-11-02 10:49:20 -0500

    updating for heroku

---
+++ client/build/precache-manifest.7b24493a5037913a4bfd6e590b486b44.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45167e1e3c61cac7613320ded171b3fe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2fa9ff8f1f9821001532",
+    "url": "/static/css/main.6f7fb204.chunk.css"
+  },
+  {
+    "revision": "6570ab765b1b41d3a277",
+    "url": "/static/js/2.a23a43a3.chunk.js"
+  },
+  {
+    "revision": "2fa9ff8f1f9821001532",
+    "url": "/static/js/main.aab2f444.chunk.js"
+  },
+  {
+    "revision": "695431b8ddf3d49f9b70",
+    "url": "/static/js/runtime-main.794d3d36.js"
+  },
+  {
+    "revision": "0c1daa6342a29df95dafd27814ed8408",
+    "url": "/static/media/friends.0c1daa63.jpg"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "c3d87e0c487c3b3a7f4a5b0c5b4a8231",
+    "url": "/static/media/maintainlogo.c3d87e0c.png"
+  },
+  {
+    "revision": "ea0de9a82e1a906876408b6d5e7bfe74",
+    "url": "/static/media/user.ea0de9a8.png"
+  }
+]);
