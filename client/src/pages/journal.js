aded13e65f25c0dbcb818ed5320553ae2c9e1bd4
Maha Malampati <mahashevata@gmail.com>
2019-11-04 17:09:59 -0800

    testing heroku deployment and some authentication stuff

--- client/src/pages/journal.js
+++ client/src/pages/journal.js
@@ -1,2 +1,33 @@
-import React, { Component } from 'react';
-import PageWrapper from '../components/PageWrapper';
+import React, {Component} from 'react';
+
+import {Redirect} from 'react-router-dom';
+class JOURNAL extends Component {
+
+constructor(props){
+    super(props);
+    this.state = {
+       name:'',
+       redirect: false,
+   };
+}
+
+componentDidMount() {
+     let data = JSON.parse(sessionStorage.getItem('userData'));
+     console.log(data);
+     this.setState({name: data.userData.name})
+}
+
+render() {
+
+if(!sessionStorage.getItem('userData') || this.state.redirect){
+    return (<Redirect to={'/'}/>)
+}
+
+return (
+<div >
+Welcome {this.state.name}
+</div>
+);
+}
+}
+export default JOURNAL;
--- client/src/pages/profileLogin.js
+++ client/src/pages/profileLogin.js
@@ -1,2 +1,76 @@
-import React, { Component } from 'react';
-import PageWrapper from '../components/PageWrapper';
+import React, {Component} from 'react';
+
+import GoogleLogin from 'react-google-login';
+import {PostData} from '../services/postData';
+import {Redirect} from 'react-router-dom';
+const googleClientId = process.env.googleid;
+
+class LOGIN extends Component {
+constructor(props) {
+    super(props);
+       this.state = {
+       loginError: false,
+       redirect: false
+};
+this.signup = this.signup.bind(this);
+}
+
+signup(res, type) {
+  let postData;
+    if (type === 'google' && res.w3.U3) {
+    postData = {
+      name: res.w3.ig,
+      provider: type,
+      email: res.w3.U3,
+      provider_id: res.El,
+      token: res.Zi.access_token,
+      provider_pic: res.w3.Paa
+    };
+}
+
+if (postData) {
+PostData('signup', postData).then((result) => {
+   let responseJson = result;
+   sessionStorage.setItem("userData", JSON.stringify(responseJson));
+   this.setState({redirect: true});
+});
+} else {}
+}
+
+render() {
+
+if (this.state.redirect || sessionStorage.getItem('userData')) {
+    return (<Redirect to={'/journal'}/>)
+}
+
+
+
+const responseGoogle = (response) => {
+    console.log("google console");
+    console.log(response);
+    this.signup(response, 'google');
+}
+
+return (
+
+<div className="row body">
+<div className="medium-12 columns">
+<div className="medium-12 columns">
+<h2 id="welcomeText"></h2>
+
+
+<br/><br/>
+
+<GoogleLogin
+clientId= {googleClientId}
+buttonText="Login with Google"
+onSuccess={responseGoogle}
+onFailure={responseGoogle}/>
+
+</div>
+</div>
+</div>
+);
+}
+}
+export default LOGIN;
